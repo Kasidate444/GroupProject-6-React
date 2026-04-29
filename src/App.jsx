ef2bd797a3e7272eeca66b9c9a0e4c7a4981b075
Kasidate444 <kasidatesaeeaw@gmail.com>
2026-04-29 16:40:22 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <>
-      <Nav />
       <main style={{ minHeight: "80vh", padding: "20px" }}>
-        <h1>ยินดีต้อนรับสู่ Music Store 🎵</h1>
+        <h1>ยินดีต้อนรับสู่ Audtlist 🎵</h1>
       </main>
       <Footer />
     </>
